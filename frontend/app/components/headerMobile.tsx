2540b04c3a92203d334da1bca9c6516da44ec96c
MahgoubHusien <mahgoubhhusien@gmail.com>
2024-07-31 22:57:10 -0500

    Finished Header and began on Header Mobile

--- frontend/app/components/headerMobile.tsx
+++ frontend/app/components/headerMobile.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+"use client";   
+
+import React, { ReactNode, useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { SIDENAV_ITEMS } from "@/constants";
+import { SideNavItems } from '@/types';
+import { Icon } from '@iconify/react';
+import { motion, useCycle } from 'framer-motion';
 
 const HeaderMobile = () => {
   return (
